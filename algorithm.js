23fdad6dd85779f8b51dccffc3b14d8646ab09c0
Media <95234709+Media2021@users.noreply.github.com>
2021-12-21 14:08:17 +0100

    first algorithm

---
+++ algorithm.js
@@ -0,0 +1,59 @@
+//1
+//for (let i = 1; i <=135; i++) {
+   // console.log(i);
+    
+//}
+//2
+//for (let i = 1; i <=135; i++) {
+    //console.log(i);
+   // if (i%2 !==0) {
+      //  console.log("odd", i);
+   // }
+    
+//}
+//3
+//let sum = 0;
+//for (let i = 0; i <=135; i++) {
+   // sum = sum + i;
+   // console.log('Number is:' + i + 'Sum is :' + sum);
+    
+//}
+// 4
+
+//const x = [1,4,2,12];
+//for(var i = 0 ; i<x.length ; i++){
+   // console.log(x[i]);
+//}
+// 5
+//const x = [2,-3,-1] ;
+//let max = x[0];
+//for(var i =0 ; i<x.length ; i++){
+    //if(x[i]>max){
+       // max = x[i];
+    //}
+   // console.log(max);
+//6
+//const x = [2,1,3] ;
+//var sum = 0 ;
+//for(let i=0 ; i<x.length ; i++){
+  //  sum =sum + x[i] ;
+//}
+   // console.log(sum/x.length);
+//7
+    //const x = [2,-1,4,-3];
+   // var sum = 0 ;
+    //for(var i =0 ; i<x.length ; i++){
+      //  if(x[i]>0){
+       //     x[i]=0
+       // }
+    //}
+   // console.log(x)
+    //8
+    const x = [2,-1,4,-3];
+    for(var i =0 ; i<x.length ; i++){
+        if(x[i]<0){
+            x[i]="dream";
+        }
+    }
+    console.log(x);
+
